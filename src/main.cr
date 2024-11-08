2302d90dd61a82a2f5bf4116e538e3adbb54f487
Navid M <navidmomtahen@gmail.com>
2024-11-08 12:14:57 +0000

    fix

--- src/main.cr
+++ src/main.cr
@@ -5,13 +5,13 @@ require "../src/deps"
 if ARGV.size > 0
   dep = extract_dependency(ARGV[0]).as(Dependency)
   begin
-    yaml = YAML.parse(File.read("shard.yml")).as_h
-    hash = Hash(YAML::Any, YAML::Any).new
+    yaml = YAML.parse(File.read("test.yml")).as_h
+    dependencies = yaml[YAML::Any.new("dependencies")].as_h? || Hash(YAML::Any, YAML::Any).new
     inner_hash = Hash(YAML::Any, YAML::Any).new
 
     inner_hash[YAML::Any.new("github")] = YAML::Any.new(dep.author + "/" + dep.library)
-    hash[YAML::Any.new(dep.library)] = YAML::Any.new(inner_hash)
-    yaml[YAML::Any.new("dependencies")] = YAML::Any.new(hash)
+    dependencies[YAML::Any.new(dep.library)] = YAML::Any.new(inner_hash)
+    yaml[YAML::Any.new("dependencies")] = YAML::Any.new(dependencies)
 
     puts yaml.to_yaml
   rescue e : Exception
