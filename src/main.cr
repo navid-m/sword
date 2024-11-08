2ae436ebc76aa49e5bade124d842beeae44af549
Navid M <navidmomtahen@gmail.com>
2024-11-08 12:40:37 +0000

    fix

--- src/main.cr
+++ src/main.cr
@@ -6,7 +6,8 @@ if ARGV.size > 0
   dep = extract_dependency(ARGV[0]).as(Dependency | Nil)
   if dep
     begin
-      yaml = YAML.parse(File.read("test.yml")).as_h
+      target_file_name = "test.yml"
+      yaml = YAML.parse(File.read(target_file_name)).as_h
       dependencies = yaml[YAML::Any.new("dependencies")].as_h? || Hash(YAML::Any, YAML::Any).new
       inner_hash = Hash(YAML::Any, YAML::Any).new
 
@@ -14,7 +15,9 @@ if ARGV.size > 0
       dependencies[YAML::Any.new(dep.library)] = YAML::Any.new(inner_hash)
       yaml[YAML::Any.new("dependencies")] = YAML::Any.new(dependencies)
 
-      puts yaml.to_yaml
+      File.open(target_file_name, "w") do |shard_yml_file|
+        shard_yml_file.print yaml.to_yaml.to_s.lchop("---")
+      end
     rescue e : Exception
       puts "Something went wrong: ", e
     end
