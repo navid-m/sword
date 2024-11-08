9ceb7f870dbd851ee7821a2c29a363ebe95c9f19
Navid M <navidmomtahen@gmail.com>
2024-11-08 12:27:41 +0000

    fixes

--- src/main.cr
+++ src/main.cr
@@ -3,18 +3,20 @@ require "../src/mods"
 require "../src/deps"
 
 if ARGV.size > 0
-  dep = extract_dependency(ARGV[0]).as(Dependency)
-  begin
-    yaml = YAML.parse(File.read("test.yml")).as_h
-    dependencies = yaml[YAML::Any.new("dependencies")].as_h? || Hash(YAML::Any, YAML::Any).new
-    inner_hash = Hash(YAML::Any, YAML::Any).new
+  dep = extract_dependency(ARGV[0]).as(Dependency | Nil)
+  if dep
+    begin
+      yaml = YAML.parse(File.read("test.yml")).as_h
+      dependencies = yaml[YAML::Any.new("dependencies")].as_h? || Hash(YAML::Any, YAML::Any).new
+      inner_hash = Hash(YAML::Any, YAML::Any).new
 
-    inner_hash[YAML::Any.new("github")] = YAML::Any.new(dep.author + "/" + dep.library)
-    dependencies[YAML::Any.new(dep.library)] = YAML::Any.new(inner_hash)
-    yaml[YAML::Any.new("dependencies")] = YAML::Any.new(dependencies)
+      inner_hash[YAML::Any.new(dep.source)] = YAML::Any.new(dep.author + "/" + dep.library)
+      dependencies[YAML::Any.new(dep.library)] = YAML::Any.new(inner_hash)
+      yaml[YAML::Any.new("dependencies")] = YAML::Any.new(dependencies)
 
-    puts yaml.to_yaml
-  rescue e : Exception
-    puts "Something went wrong: ", e
+      puts yaml.to_yaml
+    rescue e : Exception
+      puts "Something went wrong: ", e
+    end
   end
 end
