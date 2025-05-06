d5721e35c92d0cfab7a8bd432c78fe8496c64370
Navid M <navidmomtahen@gmail.com>
2025-05-06 03:04:30 +0100

    fmt

--- src/main.cr
+++ src/main.cr
@@ -4,11 +4,11 @@ require "uri"
 def github_to_dependency(url : String) : NamedTuple(name: String, repo: String)
     uri = URI.parse(url)
     unless uri.host == "github.com"
-        abort "non-gh url"
+        abort "Non-GH URL."
     end
     parts = uri.path.split("/").reject(&.empty?)
     if parts.size < 2
-        abort "invalid git url format"
+        abort "Invalid git url format"
     end
     name = parts.last.downcase
     repo = "#{parts[0]}/#{parts[1]}"
@@ -20,7 +20,7 @@ pkgfile = "shard.yml"
 begin
     yaml_raw = YAML.parse(File.read(pkgfile))
 rescue
-    abort "no #{pkgfile} was found in the current directory"
+    abort "No #{pkgfile} was found in the current directory."
 end
 
 yaml = yaml_raw.as_h
@@ -31,23 +31,23 @@ if ARGV.size != 2 || !(flag = ARGV[0]).in?({"-get", "-rm"})
     abort "usage: knife -get|-rm <package-url>"
 end
 
-flag = ARGV[0]
-url = ARGV[1]
-dep = github_to_dependency(url)
+flag         = ARGV[0]
+url          = ARGV[1]
+dep          = github_to_dependency(url)
 dep_name_key = YAML::Any.new(dep[:name])
 
 if flag == "-get"
-    dep_entry = {YAML::Any.new("github") => YAML::Any.new(dep[:repo])}
-    deps[dep_name_key] = YAML::Any.new(dep_entry)
-    yaml[deps_key] = YAML::Any.new(deps)
+    dep_entry           = {YAML::Any.new("github") => YAML::Any.new(dep[:repo])}
+    deps[dep_name_key]  = YAML::Any.new(dep_entry)
+    yaml[deps_key]      = YAML::Any.new(deps)
     File.write(pkgfile, YAML::Any.new(yaml).to_yaml)
-    puts "✅ added dependency #{dep[:name]} from #{dep[:repo]}"
+    puts "✅ Added dependency #{dep[:name]} from #{dep[:repo]}."
 elsif flag == "-rm"
     if deps.delete(dep_name_key)
         yaml[deps_key] = YAML::Any.new(deps)
         File.write(pkgfile, YAML::Any.new(yaml).to_yaml)
-        puts "🗑️ removed dependency #{dep[:name]}"
+        puts "🗑️ Removed dependency #{dep[:name]}."
     else
-        puts "dependency: #{dep[:name]} not found, nothing to remove."
+        puts "Dependency: #{dep[:name]} not found, nothing to remove."
     end
 end
