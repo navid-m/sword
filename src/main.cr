81b427e5a5fa4d23eeaa8401887031544cef1e2d
Navid M <navidmomtahen@gmail.com>
2025-05-06 13:59:29 +0100

    fix

--- src/main.cr
+++ src/main.cr
@@ -14,23 +14,40 @@ def git_url_to_dependency(url : String) : NamedTuple(name: String, repo: String,
     unless provider
         abort "Unsupported git host: #{uri.host}"
     end
-
     parts = uri.path.split("/").reject(&.empty?)
     if parts.size < 2
         abort "Invalid git URL format"
     end
-
     name = parts.last.downcase
     repo = "#{parts[0]}/#{parts[1]}"
     return {name: name, repo: repo, provider: provider}
 end
 
+def prune : Bool
+    if shards_available?
+        system("shards prune")
+        puts "Dependencies cleaned."
+        return true
+    end
+    false
+end
+
 def update_and_prune
     if shards_available?
+        prune()
         system("shards update")
-        system("shards prune")
     else
         puts "shards executable not available in path, skipping update and prune."
+    end
+end
+
+def build_project(args : Array(String) = [] of String)
+    if shards_available?
+        build_command = ["shards", "build"]
+        build_command.concat(args)
+        system(build_command.join(" "))
+    else
+        puts "shards executable not available in path, skipping build."
     end
 end
 
@@ -52,11 +69,25 @@ def shards_available?
     {% end %}
 end
 
-if ARGV.size == 1 && ARGV[0] == "up"
-    update_and_prune()
-    abort
-elsif ARGV.size != 2 || !(flag = ARGV[0]).in?({"get", "rm"})
-    abort "usage: sword get|rm|up <package-url>"
+if ARGV.size >= 1
+    case ARGV[0]
+    when "up"
+        update_and_prune()
+        abort
+    when "tidy"
+        if !prune()
+            puts "Shards executable was not available, nothing tidied."
+        end
+        abort
+    when "b"
+        build_args = ARGV.size > 1 ? ARGV[1..-1] : [] of String
+        build_project(build_args)
+        abort
+    end
+end
+
+if ARGV.size != 2 || !(flag = ARGV[0]).in?({"get", "rm"})
+    abort "usage: sword get|rm|up|b <package-url>\n       sword b [build-args]"
 end
 
 begin
@@ -72,7 +103,6 @@ flag     = ARGV[0]
 url      = ARGV[1]
 dep      = git_url_to_dependency(url)
 ran      = false
-
 dep_name_key = YAML::Any.new(dep[:name])
 
 if flag == "get"
