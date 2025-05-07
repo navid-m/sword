0aadbe802b96159f6e604307843df79a69b9073a
Navid M <navidmomtahen@gmail.com>
2025-05-07 09:57:34 +0100

    fix

--- src/main.cr
+++ src/main.cr
@@ -44,6 +44,18 @@ def load_shard_yml : Hash(YAML::Any, YAML::Any)
     end
 end
 
+def read_shard_yml : Array(String)
+    begin
+        File.read_lines(PKGFILE)
+    rescue
+        abort "No #{PKGFILE} was found in the current directory."
+    end
+end
+
+def write_shard_yml(lines : Array(String))
+    File.write(PKGFILE, lines.join("\n"))
+end
+
 def search_packages(query : String)
     print_info "Searching for packages matching '#{query}'..."
 
@@ -106,27 +118,76 @@ def show_dependency_tree
 end
 
 def add_dependency(url : String, version : String? = nil)
-    yaml         = load_shard_yml
-    deps_key     = YAML::Any.new("dependencies")
-    deps         = yaml[deps_key]?.try(&.as_h) || {} of YAML::Any => YAML::Any
-    dep          = git_url_to_dependency(url)
-    dep_name_key = YAML::Any.new(dep[:name])
-
+    dep = git_url_to_dependency(url)
     print_info "Adding dependency: #{dep[:name]} from #{dep[:provider]}: #{dep[:repo]}"
+    lines                       = read_shard_yml()
+    dependencies_index          = -1
+    dependencies_indentation    = ""
 
-    dep_entry = {YAML::Any.new(dep[:provider]) => YAML::Any.new(dep[:repo])}
-
-    if version
-        dep_entry[YAML::Any.new("version")] = YAML::Any.new(version)
+    lines.each_with_index do |line, index|
+        if line =~ /^(\s*)dependencies\s*:/
+            dependencies_index = index
+            dependencies_indentation = $1
+            break
+        end
     end
 
-    deps[dep_name_key]  = YAML::Any.new(dep_entry)
-    yaml[deps_key]      = YAML::Any.new(deps)
+    if dependencies_index == -1
+        lines << "" if lines.last != ""
+        lines << "dependencies:"
+        dependencies_index = lines.size - 1
+        dependencies_indentation = ""
+    end
 
-    File.write(
-        PKGFILE,
-        YAML::Any.new(yaml).to_yaml
-    )
+    dep_name = dep[:name]
+    dep_start_index = -1
+    dep_end_index = -1
+
+    (dependencies_index + 1).upto(lines.size - 1) do |i|
+        break if i >= lines.size || lines[i] =~ /^\S/ && !lines[i].starts_with?("#")
+        if lines[i] =~ /^\s+#{Regex.escape(dep_name)}\s*:/
+            dep_start_index = i
+
+            j = i + 1
+            while j < lines.size && (lines[j].empty? || lines[j].starts_with?("#") || lines[j] =~ /^\s+/)
+                if lines[j] =~ /^(\s+)/ && $1.size > lines[i].index(/\S/).not_nil!
+                    dep_end_index = j
+                end
+                j += 1
+            end
+
+            break
+        end
+    end
+
+    dep_indentation  = "#{dependencies_indentation}  "
+    prop_indentation = "#{dependencies_indentation}    "
+
+    dep_lines = ["#{dep_indentation}#{dep_name}:"]
+    dep_lines << "#{prop_indentation}#{dep[:provider]}: #{dep[:repo]}"
+    dep_lines << "#{prop_indentation}version: #{version}" if version
+
+    if dep_start_index != -1
+        lines.delete_at(dep_start_index..dep_end_index)
+        dep_lines.each_with_index do |line, idx|
+            lines.insert(dep_start_index + idx, line)
+        end
+    else
+        insert_index = dependencies_index + 1
+
+        while insert_index < lines.size &&
+              (lines[insert_index].empty? ||
+               lines[insert_index].starts_with?("#") ||
+               lines[insert_index] =~ /^\s+/)
+            insert_index += 1
+        end
+
+        dep_lines.each_with_index do |line, idx|
+            lines.insert(insert_index + idx, line)
+        end
+    end
+
+    write_shard_yml(lines)
 
     print_success "Added dependency #{dep[:name]} from #{dep[:provider]}: #{dep[:repo]}#{version ? " with version #{version}" : ""}."
 
@@ -136,16 +197,74 @@ def add_dependency(url : String, version : String? = nil)
 end
 
 def remove_dependency(url : String)
-    yaml = load_shard_yml
-    deps_key = YAML::Any.new("dependencies")
-    deps = yaml[deps_key]?.try(&.as_h) || {} of YAML::Any => YAML::Any
-
     dep = git_url_to_dependency(url)
-    dep_name_key = YAML::Any.new(dep[:name])
+    print_info "Removing dependency: #{dep[:name]}"
 
-    if deps.delete(dep_name_key)
-        yaml[deps_key] = YAML::Any.new(deps)
-        File.write(PKGFILE, YAML::Any.new(yaml).to_yaml)
+    lines = read_shard_yml()
+    dependencies_index = -1
+
+    lines.each_with_index do |line, index|
+        if line =~ /^(\s*)dependencies\s*:/
+            dependencies_index = index
+            break
+        end
+    end
+
+    if dependencies_index == -1
+        print_warning "Dependency: #{dep[:name]} not found, nothing to remove."
+        return
+    end
+
+    dep_name = dep[:name]
+    dep_start_index = -1
+    dep_end_index = -1
+    dep_indentation = nil
+
+    (dependencies_index + 1).upto(lines.size - 1) do |i|
+        break if i >= lines.size || (lines[i] =~ /^\S/ && !lines[i].starts_with?("#"))
+
+        if lines[i] =~ /^(\s+)#{Regex.escape(dep_name)}\s*:/
+            dep_start_index = i
+            dep_indentation = $1.size
+
+            j = i + 1
+            while j < lines.size
+                if !lines[j].empty? && !lines[j].starts_with?("#") && lines[j] =~ /^(\s*)\S/
+                    current_indent = $1.size
+                    if current_indent <= dep_indentation
+                        break
+                    end
+                    dep_end_index = j
+                end
+                j += 1
+            end
+
+            break
+        end
+    end
+
+    if dep_start_index != -1
+        if dep_end_index != -1
+            lines.delete_at(dep_start_index..dep_end_index)
+        else
+            lines.delete_at(dep_start_index)
+        end
+
+        has_other_deps = false
+        (dependencies_index + 1).upto(lines.size - 1) do |i|
+            break if i >= lines.size || (lines[i] =~ /^\S/ && !lines[i].starts_with?("#"))
+            if lines[i] =~ /^\s+\S+\s*:/
+                has_other_deps = true
+                break
+            end
+        end
+
+        if !has_other_deps
+            lines.delete_at(dependencies_index)
+        end
+
+        write_shard_yml(lines)
+
         print_success "Removed dependency #{dep[:name]}."
         if shards_available?
             update_and_prune
